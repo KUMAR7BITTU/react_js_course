a54062bf941c74cfccc452532e77450d79052bbe
Bittu <bittukumar30dec2002@gmail.com>
2023-07-06 12:59:26 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 function App() {
   return(
     <>
-     <Navbar title="TextUtils2"/>
+     <Navbar title="TextUtils2" aboutText="About TextUtils"/>
 </>    
 );
 }

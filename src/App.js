23eb1352a56bd95a499f36d172521518ca1b6c15
Bittu <bittukumar30dec2002@gmail.com>
2023-07-06 10:29:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 let name = "Bittu";
 function App() {
   return (
-    <div className="container">
-      <h1>Hello {name}</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis sed dignissimos porro accusamus quo beatae dicta soluta tenetur officia, atque debitis hic recusandae et doloribus expedita necessitatibus illo corporis harum. Nam autem a perspiciatis.
-      </p>
-    </div>
+    <div className="blank">lovely</div>
     
   );
 }

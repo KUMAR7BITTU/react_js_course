9b67a06681be39adb73c3794178f57a5ec7fc164
Bittu <bittukumar30dec2002@gmail.com>
2023-07-06 20:52:18 +0530

    Calculating time

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,8 +36,10 @@ export default function TextForm(props) {
     <div className="container my-3">
         <h1> Your text summary </h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
-
     </>
   );
 }

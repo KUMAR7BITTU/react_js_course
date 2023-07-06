680f34e92ade141e4e91d5bbf444336427d55aac
Bittu <bittukumar30dec2002@gmail.com>
2023-07-06 10:19:46 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Bittu";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Bittu kumar
-        </a>
-      </header>
-    </div>
+   <div className="container">
+
+     <h1>Hello {name}</h1>
+   </div>
   );
 }
 
